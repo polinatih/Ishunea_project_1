506951557885be929f86528baa74933b2edda1e0
Polina <tihaninap@gmail.com>
2025-09-04 17:55:53 +0300

    layout

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,11 +3,11 @@
 import { CustomButtonProps } from '@/types';
 import Image from 'next/image';
 
-const CustomButton = ({title,containerStyles,handleClick}: CustomButtonProps) => {
+const CustomButton = ({title,containerStyles,handleClick, btnType}: CustomButtonProps) => {
   return (
 <button
     disabled={false}
-    type="button"
+    type={btnType ||"button"}
     className={`bg-primary-blue text-white font-bold py-2 px-4 rounded ${containerStyles}`}
     onClick = {handleClick}
     >
